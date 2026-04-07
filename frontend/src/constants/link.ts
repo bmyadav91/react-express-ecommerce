977ad87da8f0422818e4a91bb5947618fae3f290
bmyadav91 <mannuyadavbm@gmail.com>
2026-04-07 23:15:05 +0530

    added dynamic constant links

---
+++ frontend/src/constants/link.ts
@@ -0,0 +1,5 @@
+export const constantLinks = {
+    // BASE_API_URL: "http://localhost:5000"
+
+    BASE_API_URL: "https://react-express-ecommerce.vercel.app/"
+}
--- frontend/src/services/api/axios.ts
+++ frontend/src/services/api/axios.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { constantLinks } from "../../constants/link";
 import { useAuthStore } from "../../features/auth/store/userStore";
 
 export const api = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: constantLinks?.BASE_API_URL,
     timeout: 10000, 
     headers: {
         "Content-Type": "application/json"
